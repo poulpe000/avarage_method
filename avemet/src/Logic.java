cd82dfef56d2ffaa8da5f0fd36ccd5c9b7ee9f9e
poulpe <mfsmile000@gmail.com>
2017-12-06 18:12:01 +0300

    не останавливается сразу, вылетает при вводе строк и символов

--- avemet/src/Logic.java
+++ avemet/src/Logic.java
@@ -6,15 +6,20 @@ public class Logic {
     public static void main(String []args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.print("Введите числа: ");
-
-        float result = 0;
-        if(!reader.equals("stop")) {
+        double num = 0;
+        double summ = 0;
+        int i = 0;
+        try {
             do {
-                float insertedNum = Float.parseFloat(reader.readLine());
-                result += insertedNum;
-            } while (!reader.equals("stop"));
+                 num = Double.parseDouble(reader.readLine());
+                summ += num;
+                i++;
+            } while (!reader.readLine().equals("stop"));
+            if(reader.readLine().equals("stop")){
+                System.out.println(summ/i);
+            }
+        }catch (Exception e){
+            System.out.println(summ);
         }
-        else System.out.println(result);
     }
 }
